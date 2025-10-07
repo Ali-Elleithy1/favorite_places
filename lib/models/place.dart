9b506c69ab6dcba5452f949ed7b3db76e4eb39f3
Ali Elleithy <ali.kh1615@gmail.com>
2025-10-07 23:50:33 +0300

    Add uuid to generate random ids

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,6 +1,10 @@
-class Place {
-  Place({required this.title});
+import 'package:uuid/uuid.dart';
 
-  final String id = DateTime.now().toString();
+const uuid = Uuid();
+
+class Place {
+  Place({required this.title}) : id = uuid.v4();
+
+  final String id;
   final String title;
 }
