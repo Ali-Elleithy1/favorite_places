12822e8b554909d845c456f731bdd2749c93d130
Ali Elleithy <ali.kh1615@gmail.com>
2025-10-10 19:31:57 +0300

    Add image_input widget

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() {
+    return _ImageInputState();
+  }
+}
+
+class _ImageInputState extends State<ImageInput> {
+  void _takePicture() {}
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2),
+        ),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: TextButton.icon(
+        onPressed: _takePicture,
+        label: const Text('Take Picture'),
+        icon: const Icon(Icons.camera),
+      ),
+    );
+  }
+}
